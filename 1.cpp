19f6036b00ae4e51b685c5e9b8cdf9d43e070850
KasperRovshen <147344776+KasperRovshen@users.noreply.github.com>
2024-05-07 14:38:39 +0300

    3.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,55 +1,101 @@
+/*я не знаю правильно я сделал или нет но решил сделать суффиксный автомат для строки abbab. 
+также решил поставить задачу для проверки вхождения строки.*/
+
 #include <iostream>
-#include <string>
-#include <unordered_map>
 #include <vector>
-#include <algorithm>
+#include <map>
+#include <string>
 
 using namespace std;
 
-template<typename State, typename Symbol>
-class DFA {
+template<typename StringType>
+class SuffixAutomaton {
 private:
-    vector<Symbol> alphabet;
-    unordered_map<State, unordered_map<Symbol, State>> transitionTable;
-    State initialState;
-    vector<State> finalStates;
+    struct State {
+        int length; // Длина суффикса
+        int link; // Ссылка на состояние, представляющее наибольший суффикс
+        map<typename StringType::value_type, int> next; // Переходы из текущего состояния
+
+        State() : length(0), link(-1) {}
+    };
+
+    vector<State> states;
+    int last;
 
 public:
-    DFA(vector<Symbol> alphabet, unordered_map<State, unordered_map<Symbol, State>> transitionTable, State initialState, vector<State> finalStates)
-        : alphabet(alphabet), transitionTable(transitionTable), initialState(initialState), finalStates(finalStates) {}
+    SuffixAutomaton(const StringType& s) {
+        states.emplace_back(); //начальное состояние
+        last = 0;
 
-    bool isAccepted(string input) {
-        State currentState = initialState;
-        for (char c : input) {
-            if (find(alphabet.begin(), alphabet.end(), c) == alphabet.end()) {
-                cout << "Invalid input character: " << c << endl;
+        for (auto c : s) {
+            addCharacter(c);
+        }
+    }
+
+    void addCharacter(typename StringType::value_type c) {
+        int newStateIndex = states.size();
+        states.emplace_back();
+
+        int currentState = last;
+        last = newStateIndex;
+
+        states[newStateIndex].length = states[currentState].length + 1;
+        
+        //  переход от текущего состояния к новому состоянию по символу c
+        while (currentState != -1 && states[currentState].next.find(c) == states[currentState].next.end()) {
+            states[currentState].next[c] = newStateIndex;
+            currentState = states[currentState].link;
+        }
+
+        if (currentState == -1) {
+            states[newStateIndex].link = 0;
+        } else {
+            int nextState = states[currentState].next[c];
+            if (states[currentState].length + 1 == states[nextState].length) {
+                states[newStateIndex].link = nextState;
+            } else {
+                int cloneStateIndex = states.size();//клон состояния для корректной обработки ссылок
+                states.emplace_back(states[nextState]);
+                states[cloneStateIndex].length = states[currentState].length + 1;
+
+                while (currentState != -1 && states[currentState].next[c] == nextState) {
+                    states[currentState].next[c] = cloneStateIndex;
+                    currentState = states[currentState].link;
+                }
+
+                states[newStateIndex].link = states[nextState].link = cloneStateIndex;
+            }
+        }
+    }
+    // Метод для проверки является ли строка p подстрокой s
+    bool isSubstring(const StringType& p) {
+        int currentState = 0;
+
+        for (auto c : p) {
+            if (states[currentState].next.find(c) == states[currentState].next.end()) {
                 return false;
             }
-            currentState = transitionTable[currentState][c];
+            currentState = states[currentState].next[c];
         }
-        return (find(finalStates.begin(), finalStates.end(), currentState) != finalStates.end());
+
+        return true;
     }
 };
 
+/*для проверки
+-"bab", -"aaa"*/
+
 int main() {
-    // алфавит, таблицу переходов, начальное и конечное состояния.
-    vector<char> alphabet = {'0', '1'};
-    unordered_map<char, unordered_map<char, char>> transitionTable = {
-        {'A', {{'0', 'B'}, {'1', 'A'}}},
-        {'B', {{'0', 'C'}, {'1', 'B'}}},
-        {'C', {{'0', 'A'}, {'1', 'C'}}}
-    };
-    char initialState = 'A';
-    vector<char> finalStates = {'B'};
+    string s = "abbab";
+    string p;
+    cout << "Enter string p: ";
+    cin >> p;
 
-    // экземпляр DFA
-    DFA<char, char> dfa(alphabet, transitionTable, initialState, finalStates);
-
-    // Тестовые строки на соответствие DFA
-    cout << "Testing strings against the DFA:\n";
-    vector<string> testStrings = {"","1", "0", "00", "000", "0000", "00000", "01", "10", "101", "110"};
-    for (const string& str : testStrings) {
-        cout << "String: " << str << ", Accepted: " << (dfa.isAccepted(str) ? "Yes" : "No") << endl;
+    SuffixAutomaton<string> sa(s);
+    if (sa.isSubstring(p)) {
+        cout << "String p is a substring of string s." << endl;
+    } else {
+        cout << "String p is not a substring of string s." << endl;
     }
 
     return 0;
